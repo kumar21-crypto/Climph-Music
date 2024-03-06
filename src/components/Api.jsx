b2a68bab5d11025c631c4f19ade86072cb9e0e54
Anuruddh Marskole <72235150+kumar21-crypto@users.noreply.github.com>
2024-03-06 10:10:46 +0530

    Update Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -38,7 +38,7 @@ export const endpoints = {
 export const fetchDataFromApi = async (endpoint) => {
 
     endpoint = endpoints['homeData'];
-    const { data } = await axios.get(`${CORS_URL}${BASE_URL}${endpoint}${API_STRING}`,{
+    const { data } = await axios.get(`${BASE_URL}${endpoint}${API_STRING}`,{
       headers:{
          "Access-Control-Allow-Headers":"*",
       }
@@ -48,7 +48,7 @@ export const fetchDataFromApi = async (endpoint) => {
 }
 
 export const fetchAlbumData = async (id) => {
-   const { data } = await axios.get(`${CORS_URL}${BASE_URL}${endpoints['albumDetails']}${BASE_ALBUM_ID}${id}${API_STRING}`,{
+   const { data } = await axios.get(`${BASE_URL}${endpoints['albumDetails']}${BASE_ALBUM_ID}${id}${API_STRING}`,{
       headers:{
          "Access-Control-Allow-Headers":"*",
       }
@@ -57,7 +57,7 @@ export const fetchAlbumData = async (id) => {
 }
 
 export const fetchPlaylistData = async (id) => {
-   const { data } = await axios.get(`${CORS_URL}${BASE_URL}${endpoints['playlistDetails']}${BASE_PLAYLIST_ID}${id}${API_STRING}`,{
+   const { data } = await axios.get(`${BASE_URL}${endpoints['playlistDetails']}${BASE_PLAYLIST_ID}${id}${API_STRING}`,{
       headers:{
          "Access-Control-Allow-Headers":"*",
       }
