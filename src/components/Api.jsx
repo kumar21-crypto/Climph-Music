e07f36735f1263e3b70762ffcc141090668405ba
Anuruddh Marskole <72235150+kumar21-crypto@users.noreply.github.com>
2024-03-06 14:24:44 +0530

    Update Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -15,7 +15,7 @@ const BASE_PLAYLIST_ID = "&cc=in&listid=";
 const ARTISTS_TOKEN = "&token=";
 // const CORS_URL = "https://proxy.cors.sh/";
 const CORS_URL = "https://corsproxy.io/?";
-const ALLOW_ORIGIN = "https://cors-proxy.htmldriven.com/?url=";
+const ALLOW_ORIGIN = "https://corsproxy.org/?";
 
 export const endpoints = {
     'homeData': '__call=webapi.getLaunchData',
